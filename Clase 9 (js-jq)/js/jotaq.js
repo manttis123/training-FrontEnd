8aca3e6e74a9bc6e5b72d13928be190090d90a29
John Doe <johndoe@example.com>
2016-05-13 15:56:33 -0300

    Actualizacion 13/05

---
+++ Clase 9 (js-jq)/js/jotaq.js
@@ -0,0 +1,11 @@
+//DOMContentLoad
+	
+/* on Document Ready */
+/* Siempre se recomienda poner todo dentro de function (Sólo para JQuery) */
+$(function(){
+	/*$('.col-md-8.col-xs-12').css('background-color','rgba(0,0,0,0.5)');*/
+
+	$('.col-md-12.col100').css('background-color','rgba(0,0,0,0.8)');
+});
+
+
